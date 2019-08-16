36507c1ffd5960f8188306ce4ef14b3f621eb42b
Milena Ulman <milenadev94@gmail.com>
2019-08-16 15:59:19 +0200

    Change icons, bg colors and fonts

--- index.js
+++ index.js
@@ -10,3 +10,37 @@ $(document).ready(function() {
     $(".animated-icon3").toggleClass("open");
   });
 });
+const animation = bodymovin.loadAnimation({
+  container: document.getElementById("anim"),
+  render: "svg",
+  loop: 1,
+  path: "data.json",
+  speed: 0
+});
+const computerAnim = bodymovin.loadAnimation({
+  container: document.getElementById("computer"),
+  render: "svg",
+  loop: 1,
+  path: "computer.json"
+});
+const computerPrograming = bodymovin.loadAnimation({
+  container: document.getElementById("programing"),
+  render: "svg",
+  loop: 1,
+  path: "pro.json",
+  speed: 0
+});
+const prototypeAnimation = bodymovin.loadAnimation({
+  container: document.getElementById("prototype"),
+  render: "svg",
+  loop: 1,
+  path: "prototype.json",
+  speed: 0
+});
+const loader = bodymovin.loadAnimation({
+  container: document.getElementById("loader"),
+  render: "svg",
+  loop: true,
+  path: "loader.json",
+  speed: 0
+});
