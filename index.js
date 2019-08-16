f84ab3133fe4b32b1b551c858f6bbf0b2ceee6e6
Milena Ulman <milenadev94@gmail.com>
2019-08-17 00:32:17 +0200

    Protype footer, change font-family, add progress scroll

--- index.js
+++ index.js
@@ -1,4 +1,24 @@
+window.onscroll = function() {
+  myFunction();
+};
+
+function myFunction() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height =
+    document.documentElement.scrollHeight -
+    document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("myBar").style.width = scrolled + "%";
+}
+$(document).on("DOMContentLoaded", function() {
+  $(".loader-container").show();
+});
+
+$(document).on("load", function() {
+  $(".loader-container").fadeOut();
+});
 new WOW().init();
+
 $(document).ready(function() {
   $(".first-button").on("click", function() {
     $(".animated-icon1").toggleClass("open");
@@ -13,27 +33,27 @@ $(document).ready(function() {
 const animation = bodymovin.loadAnimation({
   container: document.getElementById("anim"),
   render: "svg",
-  loop: 1,
+  loop: true,
   path: "data.json",
   speed: 0
 });
 const computerAnim = bodymovin.loadAnimation({
   container: document.getElementById("computer"),
   render: "svg",
-  loop: 1,
+  loop: true,
   path: "computer.json"
 });
 const computerPrograming = bodymovin.loadAnimation({
   container: document.getElementById("programing"),
   render: "svg",
-  loop: 1,
+  loop: true,
   path: "pro.json",
   speed: 0
 });
 const prototypeAnimation = bodymovin.loadAnimation({
   container: document.getElementById("prototype"),
   render: "svg",
-  loop: 1,
+  loop: true,
   path: "prototype.json",
   speed: 0
 });
