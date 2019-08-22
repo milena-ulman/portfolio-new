e25ad79f383fd705e6b2e32d8760ac8f03f473ca
Milena Ulman <milenadev94@gmail.com>
2019-08-22 17:04:03 +0200

    Change icons

--- index.js
+++ index.js
@@ -14,55 +14,55 @@ function myFunction() {
 $(document).on("DOMContentLoaded", function() {
   $(".loader-container").show();
 });
-
-$(document).on("load", function() {
-  $(".loader-container").fadeOut();
-});
-new WOW().init();
-
+WOW().init();
 $(document).ready(function() {
-  $(".first-button").on("click", function() {
-    $(".animated-icon1").toggleClass("open");
+  $(".mu-logo").click(function() {
+    location.reload(true);
   });
-  $(".second-button").on("click", function() {
-    $(".animated-icon2").toggleClass("open");
+  $(document).ready(function() {
+    $(".first-button").on("click", function() {
+      $(".animated-icon1").toggleClass("open");
+    });
+    $(".second-button").on("click", function() {
+      $(".animated-icon2").toggleClass("open");
+    });
+    $(".third-button").on("click", function() {
+      $(".animated-icon3").toggleClass("open");
+    });
   });
-  $(".third-button").on("click", function() {
-    $(".animated-icon3").toggleClass("open");
+
+  const animation = bodymovin.loadAnimation({
+    container: document.getElementById("anim"),
+    render: "svg",
+    loop: true,
+    path: "data.json",
+    speed: 0
+  });
+  const computerAnim = bodymovin.loadAnimation({
+    container: document.getElementById("computer"),
+    render: "svg",
+    loop: true,
+    path: "computer.json"
+  });
+  const computerPrograming = bodymovin.loadAnimation({
+    container: document.getElementById("programing"),
+    render: "svg",
+    loop: true,
+    path: "pro.json",
+    speed: 0
+  });
+  const prototypeAnimation = bodymovin.loadAnimation({
+    container: document.getElementById("prototype"),
+    render: "svg",
+    loop: true,
+    path: "prototype.json",
+    speed: 0
+  });
+  const loader = bodymovin.loadAnimation({
+    container: document.getElementById("loader"),
+    render: "svg",
+    loop: true,
+    path: "loader.json",
+    speed: 0
   });
 });
-const animation = bodymovin.loadAnimation({
-  container: document.getElementById("anim"),
-  render: "svg",
-  loop: true,
-  path: "data.json",
-  speed: 0
-});
-const computerAnim = bodymovin.loadAnimation({
-  container: document.getElementById("computer"),
-  render: "svg",
-  loop: true,
-  path: "computer.json"
-});
-const computerPrograming = bodymovin.loadAnimation({
-  container: document.getElementById("programing"),
-  render: "svg",
-  loop: true,
-  path: "pro.json",
-  speed: 0
-});
-const prototypeAnimation = bodymovin.loadAnimation({
-  container: document.getElementById("prototype"),
-  render: "svg",
-  loop: true,
-  path: "prototype.json",
-  speed: 0
-});
-const loader = bodymovin.loadAnimation({
-  container: document.getElementById("loader"),
-  render: "svg",
-  loop: true,
-  path: "loader.json",
-  speed: 0
-});
-window.ws = new WebSlides();
